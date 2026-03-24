f36e416a0801800a2b9bc2562b7832a5ff6fa3f2
natanrubenich-source <natan.rubenich@edu.sc.senai.br>
2026-03-24 14:58:50 -0300

    Adicionado as viriaveis de ambiente

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
+import "dotenv/config";
 import routerContatos from "./routes/contatosRouter.js";
 
 const app = express();
-
 app.use(express.json());
+const SERVER_PORT = process.env.SERVER_PORT;
 
 app.use("/contatos", routerContatos);
 
-app.listen(3000, () => {
-  console.log("Aplicação rodando em: http://localhost:3000");
+app.listen(SERVER_PORT, () => {
+  console.log(`Aplicação rodando em: http://localhost:${SERVER_PORT}`);
 });
